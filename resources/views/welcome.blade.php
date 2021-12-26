fa9ac36d2904dade5be342b6f223cc0d7f2e3747
Jefry <jefry5lim@gmail.com>
2021-12-26 11:39:25 +0700

    Buat master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<style>
-    a{
-        font-size: 20px;
-        font-weight: bold;
-        margin-right: 20px;
-    }
-
-    .nav-item a:hover{
-        background-color: #87ae45;
-    }
-</style>
-
+@extends('master')
+@section('title', 'Home')
+@section('isi')
 <body>
 
     <!-- Navigation -->
@@ -63,4 +44,4 @@
     </footer>
     
 </body>
-</html> 
+@endsection
